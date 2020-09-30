d8cbdf4d8ed58887ad509a1d193520935e43b395
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2020-09-30 01:30:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import turtle, random, time
+
+wn = turtle.Screen()
+wn.bgcolor('white')
+SIZE = WIDTH, HEIGHT = 800, 600
+wn.setup(*SIZE)
+DELAY = 0.1
+
+dice = list(range(1, 7))
+
+colors = ['red', 'green', 'blue', 'black', 'purple', 'orange']
+
+winers = []
+
+class Player(turtle.Turtle):
+    def __init__(self, x, y, id, color='black', shape='turtle'):
+        super().__init__(shape)
+        self.hideturtle()
+        self.id = id
+        self.penup()
+        self.color(color)
+        self.setpos(x, y)
+        self.showturtle()
+        self.end_run = 0
+        self.time_elapsed = 0
+
+    def run(self):
+      if not self.end_run:
+        if random.choice([0, 1]):
+            steps = random.choice(dice) * 20
+            self.fd(steps)
+
+    def check_win(self):
+      if not self.end_run:
+        if self.xcor() >= 300:
+          winers.append(self.id)
+          self.time_elapsed = round(time.time() - start, 2)
+          print(f'Turtle id: {self.id} finished at time {self.time_elapsed}!')
+          self.end_run = 1
+
+players = []
+x = -300
+y = 200
+
+line = turtle.Turtle()
+line.hideturtle()
+line.color('gray')
+line.penup()
+line.goto(300, 300)
+line.pensize(7)
+line.pendown()
+line.goto(300, -300)
+
+for i in range(5):
+    players.append(Player(x, y, i+1, color=random.choice(colors)))
+    y -= 100
+
+start = time.time()
+
+race = True
+
+while race:
+    for p in players:
+        p.run()
+        p.check_win()
+        time.sleep(DELAY)
+        if len(winers) == len(players):
+          race = False
+
+print('Race ends. See the position reached by ours players')
+
+for idx, w in enumerate(winers):
+  print(f'#{idx+1} - Turtle id: {w}')
